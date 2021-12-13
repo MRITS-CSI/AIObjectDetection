a505ace14ba20592dfbaff4c90f767e18f885412
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2021-12-13 22:10:04 +0530

    Fixed Color Issue

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Canvas from './Components/Canvas';
+import Header from './Components/Header';
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<Canvas />
+		</>
+	);
+};
+
+export default App;
